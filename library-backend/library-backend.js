61e799443831e9965b2b444b17f441731f066586
Miska23 <miskalinden@gmail.com>
2021-01-18 20:13:58 +0200

    start exercise 8.6

--- library-backend/library-backend.js
+++ library-backend/library-backend.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -105,6 +106,15 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book]
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]! 
+      ): Book
+  }
 `
 
 const resolvers = {
@@ -129,6 +139,14 @@ const resolvers = {
 
   Author: {
     bookCount: (root => bookCountByAuthor(root.name))
+  },
+
+  Mutation: {
+    addBook: (_root, args) => {
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
+    }
   }
 }
 
